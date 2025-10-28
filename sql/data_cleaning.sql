93f010a20bce556e201118f19f78672830d69b58
Ruitao Chen <ruitaocchen@gmail.com>
2025-10-28 10:16:59 -0400

    Oops forgot to commit cleaned data

--- sql/data_cleaning.sql
+++ sql/data_cleaning.sql
@@ -6,4 +6,25 @@ FROM electronics
 )
 SELECT * FROM duplicates
 WHERE rn > 1
-ORDER BY item_id;
+ORDER BY item_id;i
+
+-- Exploring categories
+SELECT DISTINCT category FROM electronics;
+
+-- Exploring brands
+SELECT DISTINCT brand FROM electronics;
+
+-- Fixing IRULU brand inconsistencies
+UPDATE electronics
+SET brand = 'IRULU'
+WHERE brand LIKE '%iRULU%';
+
+-- Finding out what user attributes means
+SELECT DISTINCT user_attr FROM electronics;
+
+-- Checking NULL brand value entries in category
+SELECT * FROM electronics
+WHERE category = 'Portable Audio & Video';
+
+-- Downloading clean data
+SELECT * FROM electronics;
